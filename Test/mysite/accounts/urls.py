f68442553596c75a5d1998372f4a2b1af9138ae0
Point-George <Raddude196@Gmail.com>
2025-04-23 16:02:56 -0700

    Delete Test directory

--- Test/mysite/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('create/', views.create_account, name='create_account'),
-    path('', views.login_account, name='login_account'),
-
-]
-#path('exam/', views.Exam_booking, name='Exam_booking'),
