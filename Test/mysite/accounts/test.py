31ff56838b06ec22799a7aa09573cfadc00b4ff6
Point-George <Raddude196@Gmail.com>
2025-04-23 16:06:02 -0700

    Delete Test/mysite/accounts/test.py

--- Test/mysite/accounts/test.py
+++
@@ -1,9 +0,0 @@
-def quickCalc(value):
-    value += (value - 1) * 2
-    return value
-
-
-x = int(input("Enter the amount of levels in your pyramid: "))
-x = quickCalc(x)
-print("This would require a", x, "by", x, "pyramid with", x*x, "blocks")
-
